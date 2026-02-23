e66023eb3390ec132188563c45ba41b780d6b303
Ibaigont <ibaigont@gmail.com>
2026-02-23 17:37:52 +0100

    Gelaxka klasea sortu eta definitu

---
+++ SpaceInvaders/src/spaceInvaders/Gelaxka.java
@@ -0,0 +1,12 @@
+package spaceInvaders;
+
+public class Gelaxka {
+    int zabalera;
+    int altuera;
+
+    public Gelaxka(int zabalera, int altuera) {
+        this.zabalera = zabalera;
+        this.altuera = altuera;
+    }
+    
+}
