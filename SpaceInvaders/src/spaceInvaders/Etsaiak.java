0d20253a8f8912b036d3daf982f72007565f9bed
LanderV <lvilla010@ikasle.ehu.eus>
2026-03-02 19:19:55 +0100

    Make Ontzia abstract; move mugitu to JokalariOntzi; implement mugitu for Etsaiak; update MatrizeEredua to use JokalariOntzi

--- SpaceInvaders/src/spaceInvaders/Etsaiak.java
+++ SpaceInvaders/src/spaceInvaders/Etsaiak.java
@@ -7,6 +7,27 @@ public class Etsaiak extends Ontzia {
         super(hasieraX, hasieraY, minX, maxX, minY, maxY);
     }
 
+    @Override
+    public void mugitu(String norabidea) {
+        int x = getX();
+        int y = getY();
+        int abiadura = getAbiadura();
+
+        if (norabidea.equals("EZKERRA") && x > getMinX()) {
+            x -= abiadura;
+        } else if (norabidea.equals("ESKUINA") && x < getMaxX()) {
+            x += abiadura;
+        } else if (norabidea.equals("GORA") && y > getMinY()) {
+            y -= abiadura;
+        } else if (norabidea.equals("BEHERA") && y < getMaxY()) {
+            y += abiadura;
+        }
+
+        setX(x);
+        setY(y);
+        update();
+    }
+
     // Additional enemy-specific behavior can be added here
     public void itsasiakBehavior() {
         // ...existing code...
