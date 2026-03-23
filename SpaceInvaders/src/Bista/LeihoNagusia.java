66514dde2f7f34337a57c7933b28917ebf1c8129
ferbernachi <afernandez591@ikasle.ehu.eus>
2026-03-23 15:53:13 +0100

    Thread kendu

--- SpaceInvaders/src/Bista/LeihoNagusia.java
+++ SpaceInvaders/src/Bista/LeihoNagusia.java
@@ -115,10 +115,7 @@ public class LeihoNagusia extends JFrame implements Observer, ActionListener, Ke
     public void actionPerformed(ActionEvent e) {
         if ("JOLASTU".equals(e.getActionCommand())) {
             kartaDiseinua.show(kartaPanela, "JOKOA");
-            
-            new Thread(() -> {
-                JokoKudeaketa.getJokoKudeaketa().hasieratuJokoa();
-            }, "Inicializa-Jokoa-Thread").start();
+            JokoKudeaketa.getJokoKudeaketa().hasieratuJokoa();
         }
     }
 
