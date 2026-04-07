c7be5c8ac769e3e6f59a2c3d2c75cbe50b88bb47
Oier <ozabalegi001@ikasle.ehu.eus>
2026-04-07 11:44:28 +0200

    Ontzien diseinua konponduta.

--- SpaceInvaders/src/Eredua/FormaBerdea.java
+++ SpaceInvaders/src/Eredua/FormaBerdea.java
@@ -6,14 +6,14 @@ public class FormaBerdea implements OntziPortaera {
 
     public List<Gelaxka> ontziMota(int x, int y) {
         List<Gelaxka> formak = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 5; i++) {
             formak.add(new Gelaxka(x + i, y));
         }
-        for (int i = 1; i < 5; i++) {
+        for (int i = 1; i < 4; i++) {
             formak.add(new Gelaxka(x + i, y - 1));
         }
         formak.add(new Gelaxka(x + 1, y - 2));
-        formak.add(new Gelaxka(x + 4, y - 2)); 
+        formak.add(new Gelaxka(x + 3, y - 2)); 
         
         return formak; 
     }
--- SpaceInvaders/src/Eredua/FormaGorria.java
+++ SpaceInvaders/src/Eredua/FormaGorria.java
@@ -6,14 +6,14 @@ import java.util.List;
 public class FormaGorria implements OntziPortaera {
 	public List<Gelaxka> ontziMota(int x, int y) {
         List<Gelaxka> formak = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 3; i++) {
             formak.add(new Gelaxka(x + i, y));
         }
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 3; i++) {
             formak.add(new Gelaxka(x + i, y - 1));
         }
         formak.add(new Gelaxka(x, y - 2));
-        formak.add(new Gelaxka(x + 3, y - 2));
+        formak.add(new Gelaxka(x + 2, y - 2));
         
         return formak;
     }
